a8a7ecbea4a54aeb67e0c50d176776b13e3b7d7f
Joahan Moise Jordan BAMELA ZÔ'Ô <joahan@l-agence.digital>
2025-05-08 15:37:30 +0100

    `feat: Update Angular project to use Bootstrap 5 and ngx-bootstrap`

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { ViewContainerRef  } from '@angular/core';
+import { ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
+  
   public isLoggedIn: boolean = true;
+  public isPost: boolean = true;
+  public username = 'TestUser';
   
   title = 'Blog_ui';
 }
--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,20 +1,38 @@
-import { Component } from '@angular/core';
+import {   Component} from '@angular/core';
 import { Input } from '@angular/core';
+import { ViewEncapsulation } from '@angular/core';
+
+
 
 
 @Component({  // selector: 'app-navbar',
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class NavbarComponent {
 
   @Input() username?: string;
+  @Input() isPost?: boolean;
   public title = 'TestBlog';
-  constructor() { }
+  public menu = [
+    { name: 'Posts', link: '/posts' },
+    { name: 'Pages', link: '/pages' },
+    { name: 'Comments', link: '/comments' },
+    { name : 'Profile', link: '/Me' },
+    { name: 'Logout', link: '/logout' }
 
+  ];
+  
+  constructor() {
+   
+  }
+  
   public Search() {
     console.log('Search');
   }
 
+    
 }
+
